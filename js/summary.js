f421f65e447fc5b9fd48f4d335d038fa5b243d28
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2023-04-22 23:43:59 +0200

    deleted functions for toDoArray

--- js/summary.js
+++ js/summary.js
@@ -2,6 +2,7 @@ function initSummary() {
     includeHTML();
     greet();
     setCurrentDate();
+    // updateTaskCounts(window.toDoArray);
 }
 
 function getGreeting() {
@@ -26,43 +27,15 @@ function greet() {
     greetingElement.innerHTML = getGreeting();
 
     // Retrieve the user's name from localStorage
-    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    if (currentUser) {
-        const userElement = document.getElementById('user');
-        userElement.innerHTML = currentUser.name;
-    }
+    // const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    // if (currentUser) {
+    //     const userElement = document.getElementById('user');
+    //     userElement.innerHTML = currentUser.name;
+    // }
 }
 
 function goToBoard() {
     window.location.href = "board.html";
-}
-
-function getTaskCounts() {
-    const toDoArray = window.toDoArray;
-
-    let taskCounts = {
-        todo: 0,
-        inProgress: 0,
-        awaitingFeedback: 0,
-        done: 0
-    };
-
-    taskCounts.todo = toDoArray.filter(task => task.status === "to-do").length;
-    taskCounts.inProgress = toDoArray.filter(task => task.status === "in-progress").length;
-    taskCounts.awaitingFeedback = toDoArray.filter(task => task.status === "awaiting-feedback").length;
-    taskCounts.done = toDoArray.filter(task => task.status === "done").length;
-
-    return taskCounts;
-}
-
-function updateTaskCounts() {
-    const taskCounts = getTaskCounts(toDoArray);
-
-    document.querySelector(".summary-content-big-number[data-task='total']").textContent = toDoArray.length;
-    document.querySelector(".summary-content-big-number[data-task='in-progress']").textContent = taskCounts.inProgress;
-    document.querySelector(".summary-content-big-number[data-task='awaiting-feedback']").textContent = taskCounts.awaitingFeedback;
-    document.querySelector(".summary-content-big-number[data-task='to-do']").textContent = taskCounts.todo;
-    document.querySelector(".summary-content-big-number[data-task='done']").textContent = taskCounts.done;
 }
 
 function getMonthName(monthIndex) {
@@ -83,6 +56,8 @@ function setCurrentDate() {
     let fullDate = `${month} ${day}, ${year}`;
     document.getElementById('currentDate').innerHTML = fullDate;
 }
+
+// muss Fall noch abfangen, falls keine Deadline
 
 
 
