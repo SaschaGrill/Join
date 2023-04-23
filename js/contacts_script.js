c55236cb54a91d06b3a158ec7d156ba9b5e123b6
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2023-04-23 23:10:27 +0200

    added code to contacts.js

--- js/contacts_script.js
+++ js/contacts_script.js
@@ -14,3 +14,33 @@ let contacts = [{
     email: "simsek95@yahoo.de",
     phone: "+49 123141 11 1",
 }]
+
+
+
+
+// von Gloria eingefügt
+
+// generiert zufällige Farbe
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Wandelt erste Buchstaben von Vor- und Nachname in Initialen um
+function getColorForName(firstName, lastName) {
+    const initials = firstName.charAt(0) + lastName.charAt(0);
+    return getColorForInitials(initials);
+}
+
+// wandelt Initialen in Farben um
+function getColorForInitials(initials) {
+    const colors = {};
+    if (!colors[initials]) {
+        colors[initials] = getRandomColor();
+    }
+    return colors[initials];
+}
