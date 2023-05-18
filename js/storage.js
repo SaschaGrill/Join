6c834789ed606c044bf14eee03f7a8f38e8f2a79
SaschaGrill <aasascha@hotmail.com>
2023-05-18 08:30:04 +0200

    jsDoc implemented

--- js/storage.js
+++ js/storage.js
@@ -2,18 +2,36 @@ const STORAGE_TOKEN = '5FUFRM30ZGT2TL1U71ILFGSO4KYDUCQ5ROAYXNGA';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
 
+/**
+ * Sets an item in the storage.
+ * 
+ * @param {string} key - The key of the item.
+ * @param {string} value - The value to be stored.
+ * @returns {Promise<object>} - A promise that resolves to the response object.
+ */
 async function setItem(key, value) {
     const payload = { key, value, token: STORAGE_TOKEN };
     return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
-        .then(res => res.json());
-}
-
-async function getItem(key) {
+      .then(res => res.json());
+  }
+  
+  
+  /**
+   * Retrieves an item from the storage.
+   * 
+   * @param {string} key - The key of the item to retrieve.
+   * @returns {Promise<string>} - A promise that resolves to the retrieved value.
+   * @throws {string} - Throws an error if the requested data is not found.
+   */
+  async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    return fetch(url).then(res => res.json()).then(res => {
-        // Verbesserter code
+    return fetch(url)
+      .then(res => res.json())
+      .then(res => {
         if (res.data) {
-            return res.data.value;
-        } throw `Could not find data with key "${key}".`;
-    });
-}
+          return res.data.value;
+        }
+        throw `Could not find data with key "${key}".`;
+      });
+  }
+  
